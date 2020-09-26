c6ef94d485d63a95c5ce23012cd247d9372c8c3a
Logan <Lwalsh742@gmail.com>
2020-09-26 10:03:53 -0400

    PWA base code added

--- sw.js
+++ sw.js
@@ -1,62 +1,25 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
-
-
-var cacheName = 'CSv2';
-
-var cachedFiles = [
-
-    '/assets/dist/plugins.min.css',
-    '/assets/dist/plugins.min.js',
-    '/assets/dist/style.min.css',
-    '/assets/fonts/fontawesome-webfont.woff',
-    '/assets/js/aos.js',
-    '/assets/js/scripts.js',
-    '/assets/js/theia-sidebar.min.js',
-    '/assets/js/prism.min.js',
-
-
-
-];
-
-self.addEventListener('install', function(evt){
-    console.log('Service Worker Install Event');
-    //Add the file to the cache
-    evt.waitUntil(
-        caches.open(cacheName).then(function(cache){
-            console.log('Caching Files');
-            return cache.addAll(cachedFiles);
-        }).then(function(){
-            return self.skipWaiting();
-        }).catch(function(err){
-            console.log('Cache Failed', err);
-        })
-    );
+self.addEventListener('install', function(e) {
+ e.waitUntil(
+   caches.open('fox-store').then(function(cache) {
+     return cache.addAll([
+       '/pwa-examples/a2hs/',
+       '/pwa-examples/a2hs/index.html',
+       '/pwa-examples/a2hs/index.js',
+       '/pwa-examples/a2hs/style.css',
+       '/pwa-examples/a2hs/images/fox1.jpg',
+       '/pwa-examples/a2hs/images/fox2.jpg',
+       '/pwa-examples/a2hs/images/fox3.jpg',
+       '/pwa-examples/a2hs/images/fox4.jpg'
+     ]);
+   })
+ );
 });
 
-self.addEventListener('activate', function(evt){
-    console.log('Service Worker Activated');
-    evt.waitUntil(
-        caches.keys().then(function(keyList){
-            return Promise.all(keyList.map(function(key){
-                if(key !== cacheName){
-                    console.log('Removing Old Cache', key);
-                    return caches.delete(key)
-                }
-            }));
-        })
-    );
-    return self.clients.claim();
-});
-
-self.addEventListener('fetch', function(evt){
-    console.log('Fetch Event' + evt.request.url);
-    evt.respondWith(
-        caches.match(evt.request).then(function(response){
-            return response || fetch(evt.request);
-        })
-    );
+self.addEventListener('fetch', function(e) {
+  console.log(e.request.url);
+  e.respondWith(
+    caches.match(e.request).then(function(response) {
+      return response || fetch(e.request);
+    })
+  );
 });
